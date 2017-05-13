c89eb02c290cae715ab4f609f1d110fe9a2c9513
2e2a <alexej@2e2a.de>
2017-05-13 09:05:48 +0200

    login and logout

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,10 +1,11 @@
+from django.conf.urls import include
 from django.conf.urls import url
 
 from . import views
 
 
 urlpatterns = [
-    url(r'',
+    url(r'^$',
         views.HomeView.as_view(),
         name='home'),
 ]
--- lrex/urls.py
+++ lrex/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from apps.home import urls as home_urls
 from apps.setup import urls as setup_urls
@@ -9,5 +10,13 @@ from apps.setup import urls as setup_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^setup/', include(setup_urls)),
+
+    url(r'^accounts/login/',
+        auth_views.login,
+        {'template_name': 'accounts/login.html'}),
+    url(r'^accounts/logout/',
+        auth_views.login,
+        {'template_name': 'accounts/logged_out.html'}),
+
     url(r'', include(home_urls)),
 ]
