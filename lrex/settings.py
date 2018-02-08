4927e3c23594c390bc961d45dafb1efc03ab84c3
2e2a <alexej@2e2a.de>
2018-02-08 10:25:32 +0100

    switch to postgresql

--- lrex/settings.py
+++ lrex/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# Import local settings
+from .local import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -85,8 +88,10 @@ WSGI_APPLICATION = 'lrex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PSQL_DB,
+        'USER': PSQL_USER,
+        'PASSWORD': PSQL_PASSWORD,
     }
 }
 
