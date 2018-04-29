b461a860873b723b93bfdfc29b76defa335053ab
2e2a <alexej@2e2a.de>
2018-04-29 10:27:29 +0200

    Use sqlite per default

--- lrex/settings.py
+++ lrex/settings.py
@@ -84,7 +84,13 @@ WSGI_APPLICATION = 'lrex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # User
 LOGIN_REDIRECT_URL = '/study/'
