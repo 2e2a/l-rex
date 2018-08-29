e91f8d5a9417c1c5bb0c57d669e4fd7e9ea5fc20
2e2a <alexej@2e2a.de>
2018-08-29 09:47:56 +0200

    set breadcrumbs on home views

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,6 +5,9 @@ from django.views import generic
 class HomeView(generic.TemplateView):
     template_name = 'lrex_home/home.html'
     title = 'L-Rex: linguistic rating experiments'
+    @property
+    def breadcrumbs(self):
+        return []
 
 
 class ImprintView(generic.TemplateView):
@@ -15,3 +18,7 @@ class ImprintView(generic.TemplateView):
         data = super().get_context_data(**kwargs)
         data['imprint'] = settings.IMPRINT
         return data
+
+    @property
+    def breadcrumbs(self):
+        return []
