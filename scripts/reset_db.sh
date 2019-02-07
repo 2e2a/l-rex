090d37322a13d7a6f78b402c5f69297b6dab518e
2e2a <alexej@2e2a.de>
2019-02-07 11:43:03 +0100

    add sqlite support for reset_db script

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -7,8 +7,14 @@ read ANSWER
 if [ "$ANSWER" != "y" ]; then
     exit
 fi
-DATABASE="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,NAME)"
-USER="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,USER)"
-psql -U postgres -c "DROP DATABASE $DATABASE"
-psql -U postgres -c "CREATE DATABASE $DATABASE OWNER $USER"
-
+ENGINE="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,ENGINE)"
+if [ "$ENGINE" == "django.db.backends.postgresql" ]; then
+    DATABASE="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,NAME)"
+    USER="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,USER)"
+    psql -U postgres -c "DROP DATABASE $DATABASE"
+    psql -U postgres -c "CREATE DATABASE $DATABASE OWNER $USER"
+elif [ "$ENGINE" == "django.db.backends.sqlite3" ]; then
+    FILE="$($VIRTUAL_ENV/bin/python manage.py get_settings_value DATABASES default,NAME)"
+    rm -f $FILE
+    echo rm -f $FILE
+fi
