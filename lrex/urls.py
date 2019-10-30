58ef8ce9261e3cdc24beb657d2915625dbe7dcae
2e2a <alexej@2e2a.de>
2019-10-30 10:50:40 +0100

    Fixup

--- lrex/urls.py
+++ lrex/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 from apps.home import urls as home_urls
-from apps.experiment import urls as experiment_urls
 from apps.item import urls as item_urls
 from apps.materials import urls as materials_urls
 from apps.study import urls as study_urls
@@ -12,7 +11,6 @@ from apps.trial import urls as trial_urls
 
 urlpatterns = [
     path('studies/', include(study_urls)),
-    path('experiments/', include(experiment_urls)),
     path('materials/', include(materials_urls)),
     path('items/', include(item_urls)),
     path('trials/', include(trial_urls.urlpatterns)),
