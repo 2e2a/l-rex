f1a18106e9cc6f8fd896407f6f3ab6f0b703c666
2e2a <alexej@2e2a.de>
2019-11-26 16:34:08 +0100

    Do not warn on submit

--- lrex/assets/js/leave_warning.js
+++ lrex/assets/js/leave_warning.js
@@ -1,12 +1,17 @@
 var warn = false;
+var isSubmit = false;
 $(document).ready(function(){
     document.querySelectorAll('form').forEach(function (form) {
         $(form).data('serialize', $(form).serialize());
+        form.addEventListener('submit', function () {
+            isSubmit = true;
+            console.log('TEST')
+        })
     });
     $(window).bind('beforeunload', function(e) {
         document.querySelectorAll('form').forEach(function (form) {
             if($(form).serialize() != $(form).data('serialize')) warn = true;
         });
-        if(warn) return true;
+        if(warn && !isSubmit) return true;
     });
 });
