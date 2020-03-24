013f0b662793c8f2fc2ea5f0ee718721d8a02280
2e2a <alexej@2e2a.de>
2020-03-24 11:11:51 +0100

    Move static to staticfiles

--- lrex/settings.py
+++ lrex/settings.py
@@ -147,14 +147,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(CONFIG_DIR, 'static'),
-    os.path.join(CONFIG_DIR, 'assets'),
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(CONFIG_DIR, 'assets'),
+    os.path.join(CONFIG_DIR, 'static'),
+]
 
 # Other
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
