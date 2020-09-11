c69b13ecc18c1508f3583162fc7fd27fedb5e86d
2e2a <alexej@2e2a.de>
2020-09-11 10:03:32 +0200

    Add announcements

--- lrex/settings.py
+++ lrex/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.home.context_processors.announcements',
             ],
         },
     },
@@ -167,6 +168,7 @@ DEFAULT_FROM_EMAIL = 'noreply@l-rex.de'
 LREX_VERSION = 0.8
 LREX_CONTACT_MD = 'Please define contact as markdown in local.py'
 LREX_PRIVACY_MD = 'Please define privacy statement as markdown in local.py'
+LREX_ANNOUNCEMENTS = []
 
 # Import local settings
 try:
